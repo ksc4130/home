e6ea95542f60ccb2ec2479d757b0f212e50c0eef
Kyle Curren <ksc4130@gmail.com>
2013-10-05 09:48:59 -0400

    init files

---
+++ server.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+var cookie  = require('cookie');
+var connect = require('connect');
+var secret = 'Askindl23@146Fscmaijnd523CXVWGN#63@#7efbsd23#$Rb';

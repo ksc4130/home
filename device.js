9c1769b58b49eadc94dcb953944cf316da6b4996
Kyle Curren <ksc4130@gmail.com>
2013-10-16 21:25:44 -0400

    switches

--- device.js
+++ device.js
@@ -81,7 +81,9 @@
                     b.pinMode(self.pin, 'in');
 
                     setInterval(function () {
+                        console.log('sw');
                         self.get(function(err, val) {
+                            console.log('sw', val, self.state);
                             if(val < self.state) {
                                 //self.toggle();
                                 self.pub('switched', self);
